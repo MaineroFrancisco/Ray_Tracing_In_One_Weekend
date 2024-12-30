448a122df33d00a52eb898ecf6708cd62e9b7a85
Francisco Mainero <mainerofrancisco@gmail.com>
2024-12-30 16:29:32 -0300

    Added constants, assuming include before other headers (ugly design)

---
+++ rtweekend.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <numbers>
+#include <memory>
+
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = std::numbers::pi; // Only C++20 and above
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"

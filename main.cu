595a5f2f0344a6abb0ccdb17f936a56ba85ac70c
Francisco Mainero <mainerofrancisco@gmail.com>
2025-09-18 12:30:36 -0300

    Update CMakeLists.txt to add CUDA, rename main.cpp to main.cu and add cuda error check

--- main.cu
+++ main.cu
@@ -10,6 +10,20 @@
 #include "extras/quad.h"
 #include "extras/texture.h"
 
+#include <cuda_runtime.h>
+
+#define checkCudaErrors(val) check_cuda((val), #val, __FILE__, __LINE__)
+void check_cuda(cudaError_t result, char const *const func, const char *const file,
+				int const line) {
+	if (result) {
+		std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " << file
+				  << ":" << line << " '" << func << "' \n";
+		// Make sure we call CUDA Device Reset before exiting
+		cudaDeviceReset();
+		exit(99);
+	}
+}
+
 void bouncing_spheres() {
 	hittable_list world;
 
